a7b4e650f813f2410e4cfd6106b20b0fddcf14c2
eblack <markblack96@gmail.com>
2019-12-30 16:08:30 -0600

    More progress on conversion script

--- db_scripts/conversion.py
+++ db_scripts/conversion.py
@@ -3,18 +3,6 @@
 
 # We need tables for:
 # Congresspersons, political party codes, votes, congresses(?)
-# ________________
-#| congressperson |
-#|----------------|
-#|bioname         |
-#|party code      |
-#------------------
-# _______________
-#|     vote      |
-#|---------------|
-#|               |
-#-----------------
-# And so on
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker 
@@ -33,8 +21,30 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 # add george washington
-george = members.loc[0]
-c = Congressperson(icpsr=int(george.icpsr), bioname=george.bioname)
-session.add(c)
+# george = members.loc[0]
+# c = Congressperson(icpsr=int(george.icpsr), bioname=george.bioname)
+# session.add(c)
+
+# create first congress
+first_congress = members.loc[members['congress'] == 1]
+for i in range(len(first_congress)):
+    con = Congressperson(icpsr=int(first_congress.loc[i].icpsr), bioname=first_congress.loc[i].bioname)
+    session.add(con)
+
+# throw all the parties into the db
+unique_parties = parties.drop_duplicates(['party_code'])[['party_code', 'party_name']].reset_index() # have to do that or the following won't run
+for i in range(len(unique_parties)):
+    p = Party(party_code=int(unique_parties.loc[i].party_code), party_name=unique_parties.loc[i].party_name)
+    session.add(p)
+
+# now we want to go through the congress and assign parties
+cons = session.query(Congressperson).all()
+ps = session.query(Party).all()
+
+for p in ps:
+    for con in cons:
+        # if con in p:  Need ta figure out how best to do this!
+        #   p.append(con)
+
 session.commit()
 session.close()
--- db_scripts/models.py
+++ db_scripts/models.py
@@ -1,16 +1,22 @@
 # Here's where we're going to place our models, for use in our DB conversion script and later in the API
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 
 Base = declarative_base()
 
+congressperson_party = Table('congressperson_party', Base.metadata,
+    Column('icpsr', Integer, ForeignKey('congressperson.icpsr')),
+    Column('party_code', Integer, ForeignKey('party.party_code')),
+    Column('congress_num', Integer, ForeignKey('congress.congress_num')),
+    Column('chamber', String)
+)
 class Congressperson(Base):
     __tablename__ = 'congressperson'
     icpsr = Column(Integer, primary_key=True) # Note: this is their ICPSR code or whatever
     bioname = Column(String)
-    parties = relationship('Party', secondary=congressperson_party, backref='')
+    parties = relationship('Party', secondary=congressperson_party, back_populates='congresspersons')
     def __repr__(self):
         return "<Congressperson(bioname='%s')>" % (self.bioname)
 
@@ -19,6 +25,7 @@ class Party(Base):
     __tablename__ = 'party'
     party_code = Column(Integer, primary_key=True)
     party_name = Column(String)
+    congresspersons = relationship('Congressperson', secondary=congressperson_party, back_populates='parties')
     def __repr__(self):
         return "<Party(party_name='%s')>" % (self.party_name)
 
@@ -27,9 +34,3 @@ class Congress(Base):
     __tablename__ = 'congress'
     congress_num = Column(Integer, primary_key=True)
 
-
-association_table = Table('congressperson_party', Base.metadata,
-    Column('icpsr', Integer, ForeignKey('congressperson.icpsr')),
-    Column('party_code', Integer, ForeignKey('party.party_code')),
-    Column('congress_num', Integer, ForeignKey('congress.congress_num'))
-)
