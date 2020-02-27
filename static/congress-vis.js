211a92c1695c0e469c9851f9b952cbfa6e9d06c0
eblack <markblack96@gmail.com>
2020-02-27 16:29:49 -0600

    Intermediary push, working on basic visualizations

--- static/congress-vis.js
+++ static/congress-vis.js
@@ -21,14 +21,30 @@ function congress(n) {
       partiesAndMembers = membersOfParties;
     }
     const partyDivs = d3.select('#app').selectAll('div .party').data(Object.keys(membersOfParties))
-    partyDivs.enter().append('div').attr("class", "party").text(function(d) { return d;})
-    
+    partyDivs.enter().append('div').attr("class", "party").text(function(d, i) { return d;})
+    let idx = 0; // time to hack!
     for (p in membersOfParties) {
       let header = document.createElement('h3');
       header.textContent = p;
       document.getElementById(p).append(header);
       d3.select('#' + p).selectAll('p').data(membersOfParties[p]).enter().append('p')
         .text(function(d) { return d.bioname; });
+      
+      let svg = d3.select('svg').append('g').attr("class", p);
+      svg.attr("transform", "translate("+idx*150+")");
+      svg.data(Object.keys(membersOfParties)).exit().remove();      
+      let congress = svg.selectAll('circle').data(membersOfParties[p])
+        .attr("cx", function(d, i) { return 10 + i%10 * 15;})
+        .attr("cy", function(d, i) { return 10 + Math.floor(i/10) * 15; })
+        .attr("r", 10+"px");
+
+      congress.enter().append("circle")
+        .attr("cx", function(d, i) { return 10 + i%10 * 15;})
+        .attr("cy", function(d, i) { return 10 + Math.floor(i/10) * 15; })
+        .attr("r", 10+"px");
+
+      congress.exit().remove();
+      idx+=1;
     }
 
     /* const congress = d3.select('#congress_holder')
