299f7cd3fb6e46b4971b06651505b2f494177465
eblack <markblack96@gmail.com>
2020-03-03 16:20:52 -0600

    Different approach

---
+++ static/house-and-senate.js
@@ -0,0 +1,75 @@
+// house and senate visualizations, separated
+
+// retrieves house & senate data from the api and displays it
+var dataGlobal;
+function congress(n) {
+    d3.json('/congress/' + n).then(function(data) {
+        dataGlobal = data;
+        let parties = listParties(data);
+        let chambers = ['House', 'Senate'];
+        // Goal: use two different svgs for house and senate SVGs
+
+        // house
+        let houseData = data.filter(function(d){
+            if (d.chamber === 'House') {
+                return d;
+            }
+        });
+        let houseMembersByParty = {};
+        for (p in parties) {
+            let party = parties[p];
+            houseMembersByParty[party] = houseData.filter(
+                function(d) { return d.party===party; }
+            )
+        }
+        let g = d3.select('.house')
+            .selectAll('g')
+            .data(parties)
+                .attr('class', function(d) { return d.replace(".", "").replace(' ', '');})
+                .attr('transform', function(d, i) {
+                    return "translate(" + i * 200 + ")"; 
+                });
+        
+        g.enter().append('g')
+            .attr('class', function(d) { return d.replace(".", "").replace(' ', '');})
+            .attr('transform', function(d, i) {
+                return "translate(" + i * 200 + ")"; 
+            });
+
+        g.exit().remove();
+
+        let houseMembers = d3.selectAll('g')
+            .data(parties).selectAll('circle')
+            .data(function(d) {return houseMembersByParty[d];}) // get party members in chamber
+                .attr('cx', function(d, i){return 10 + i%10 * 20;})
+                .attr('cy', function(d, i){return 10 + Math.floor(i/10) * 20;})
+                .attr('r', 8+'px')
+                .on('mouseover', function(d) { 
+                    d3.select(this).attr('stroke-width', 3) 
+                }).on('mouseout', function(d) { 
+                    d3.select(this).attr('stroke-width', 1) 
+                });
+        
+        houseMembers.enter().append('circle')
+            .attr('cx', function(d, i){return 10 + i%10 * 20;})
+            .attr('cy', function(d, i){return 10 + Math.floor(i/10) * 20;})
+            .attr('r', 8+'px')
+            .on('mouseover', function(d) { 
+                d3.select(this).attr('stroke-width', 3) 
+            })
+            .on('mouseout', function(d) { 
+                d3.select(this).attr('stroke-width', 1) 
+            });
+        houseMembers.exit().remove();
+
+
+    });
+}
+
+function showCongress() {
+    // get the selected option
+    let selectedIndex = document.getElementById('congress_num_select').options.selectedIndex;
+    let selectedCongress = document.getElementById('congress_num_select').options[selectedIndex].value; // hilariously verbose
+    congress(selectedCongress);
+  }
+  
