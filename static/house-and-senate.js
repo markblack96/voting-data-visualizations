ad02581e33a238ec629e6ff5e5cf004d36d4684a
eblack <markblack96@gmail.com>
2020-03-05 11:08:31 -0600

    Scrolling, senate visualization

--- static/house-and-senate.js
+++ static/house-and-senate.js
@@ -38,7 +38,7 @@ function congress(n) {
 
         g.exit().remove();
 
-        let houseMembers = d3.selectAll('g')
+        let houseMembers = d3.selectAll('svg.house g')
             .data(parties).selectAll('circle')
             .data(function(d) {return houseMembersByParty[d];}) // get party members in chamber
                 .attr('cx', function(d, i){return 10 + i%10 * 20;})
@@ -62,6 +62,58 @@ function congress(n) {
             });
         houseMembers.exit().remove();
 
+        // now do senate
+        let senateData = data.filter(function(d){
+            if (d.chamber === 'Senate') {
+                return d;
+            }
+        });
+        let senatorsByParty = {};
+        for (p in parties) {
+            let party = parties[p];
+            senatorsByParty[party] = senateData.filter(
+                function(d) { return d.party===party; }
+            )
+        }
+        g = d3.select('svg.senate')
+            .selectAll('g')
+            .data(parties)
+                .attr('class', function(d) { return d.replace(".", "").replace(' ', '');})
+                .attr('transform', function(d, i) {
+                    return "translate(" + i * 200 + ")"; 
+                });
+        
+        g.enter().append('g')
+            .attr('class', function(d) { return d.replace(".", "").replace(' ', '');})
+            .attr('transform', function(d, i) {
+                return "translate(" + i * 200 + ")"; 
+            });
+
+        g.exit().remove();
+
+        let senateMembers = d3.selectAll('svg.senate g')
+            .data(parties).selectAll('circle')
+            .data(function(d) {return senatorsByParty[d];}) // get party members in chamber
+                .attr('cx', function(d, i){return 10 + i%10 * 20;})
+                .attr('cy', function(d, i){return 10 + Math.floor(i/10) * 20;})
+                .attr('r', 8+'px')
+                .on('mouseover', function(d) { 
+                    d3.select(this).attr('stroke-width', 3) 
+                }).on('mouseout', function(d) { 
+                    d3.select(this).attr('stroke-width', 1) 
+                });
+        
+        senateMembers.enter().append('circle')
+            .attr('cx', function(d, i){return 10 + i%10 * 20;})
+            .attr('cy', function(d, i){return 10 + Math.floor(i/10) * 20;})
+            .attr('r', 8+'px')
+            .on('mouseover', function(d) { 
+                d3.select(this).attr('stroke-width', 3) 
+            })
+            .on('mouseout', function(d) { 
+                d3.select(this).attr('stroke-width', 1) 
+            });
+        senateMembers.exit().remove();
 
     });
 }
